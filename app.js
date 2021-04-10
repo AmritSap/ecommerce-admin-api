61335818b5ae89a547ac43378bf4f636c1bac16e
AmritSap <amritsapkota32@gmail.com>
2021-04-10 11:12:17 +1000

    api setup done

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import morgan from 'morgan'
+const app = express()
+import cors from 'cors'
+const PORT= process.env.PORT || 8000
+app.use(cors());
+app.use(morgan("tiny"));
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(PORT,error =>{
+  if (error)
+  console.log(error)
+  console.log(`Server is running at http://localhost:${PORT}`)
+});
